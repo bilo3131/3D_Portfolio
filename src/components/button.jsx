210d5c17acfe8a07e70208fccf2189b7a8a8d066
Bilal Alac <bilalalac@gmail.com>
2026-07-30 16:09:21 +0200

    feat: add button component, constants for navigation and content, and hero section

---
+++ src/components/button.jsx
@@ -0,0 +1,9 @@
+const Button = () => {
+    return (
+        <a className="cta-wrapper">
+            
+        </a>
+    )
+}
+
+export default Button
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,42 @@
+import { words } from "../constants"
+
+const Hero = () => {
+    return (
+        <section id="hero" className="relative overflow-hidden">
+            <div className="absolute top-0 left-0 z-10">
+                <img src="/images/bg.png" alt="background" />
+            </div>
+
+            <div className="hero-layout">
+                {/*LEFT: HERO CONTENT */}
+                <header className="flex flex-col justify-center md:w-full w-screen md:px-20 py-5">
+                    <div className="flex flex-col gap-7">
+                        <div className="hero-text">
+                            <h1>
+                                Shaping
+                                <span className="slide">
+                                    <span className="wrapper">
+                                        {words.map((word) => (
+                                            <span key={word.key} className="flex items-center md:gap-3 gap-1 pb-2">
+                                                <img src={word.imgPath} alt={word.text} className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50" />
+                                                <span>{word.text}</span>
+                                            </span>
+                                        ))}
+                                    </span>
+                                </span>
+                            </h1>
+                            <h1>into Real Projects</h1>
+                            <h1>that Deliver Results</h1>
+                        </div>
+                        <p>
+                            Hi, I'm Bilal, a developer based in Germany with a passion for code.
+                        </p>
+                    </div>
+                </header>
+                {/*RIGHT: 3D MODEL */}
+            </div>
+        </section>
+    )
+}
+
+export default Hero
