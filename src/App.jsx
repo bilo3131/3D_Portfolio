ceb40b4e5304bde17b79db397e6fffe4706689ba
Bilal Alac <bilalalac@gmail.com>
2026-08-01 15:57:42 +0200

    feat: add TechStack section with GSAP animations and integrate TechIcon component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import FeatureCards from './sections/FeatureCards.jsx';
 import Hero from './sections/Hero.jsx';
 import ShowcaseSection from './sections/ShowcaseSection.jsx';
 import ExperienceSection from './sections/Experience.jsx';
+import TechStack from './sections/TechStack.jsx';
 
 const App = () => {
     return (
@@ -14,6 +15,7 @@ const App = () => {
             <LogoShowcase />
             <FeatureCards />
             <ExperienceSection />
+            <TechStack />
         </>
     );
 };
